65d868b3667478ac33b2cc8a1139c22ac9fd0de4
mainul islam <islammuhammadmainu@gmail.com>
2025-05-05 22:43:45 +0600

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # medical-ai-chatbot
+STEPS:
+Clone the repository
+
+
+STEP 01- Create a conda environment after opening the repository
+
+conda create -n medibot python=3.10 -y
+
+conda activate medibot
+
+STEP 02- install the requirements
+pip install -r requirements.txt
+python app.py
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup 
+
+
+setup(
+    name='Medical AI Chatbot',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A medical AI chatbot that provides information and assistance related to health and medical queries.',
+    author='MAINUL ISLAM',
+    author_email='islammuhammadmainu@gmail.com',
+    license='MIT',
+)
